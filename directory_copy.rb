8c45eb74c8a212bf0bac762d1d1be6cdfea9d362
Marina Papapetrou <contact@marinapapap.com>
2022-11-03 10:33:07 +0000

    Step 8 Ex 2 & 3

--- directory_copy.rb
+++ directory_copy.rb
@@ -25,7 +25,9 @@ end
 
 def prints(students)
   students.each_with_index do |student, index|
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    if (student[:name][0] == "M" || student[:name][0] == "m") && student[:name].length < 12
+      puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
   end
 end
 
