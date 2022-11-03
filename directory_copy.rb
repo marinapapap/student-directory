043ceea1235f3ecad8dc86d30fbd4df56fe85259
Marina Papapetrou <contact@marinapapap.com>
2022-11-03 11:14:08 +0000

    Step 8 Ex 6

--- directory_copy.rb
+++ directory_copy.rb
@@ -1,7 +1,7 @@
 def input_students
-  puts "Please enter the names of the students, their country of birth, age and favourite hobby"
-  puts "To finish, just hit return twice"
-  puts "First enter their name"
+  puts "Please enter the names of the students, their country of birth, age and favourite hobby".center(70)
+  puts "To finish, just hit return twice".center(70)
+  puts "First enter their name".center(70)
   
   students = []
   
@@ -9,19 +9,19 @@ def input_students
   
   while !name.empty? do
     
-    puts "Enter their country of birth"
+    puts "Enter their country of birth".center(70)
     cob = gets.chomp
     
-    puts "Enter their age"
+    puts "Enter their age".center(70)
     age = gets.chomp
     
-    puts "Enter their favourite hobby"
+    puts "Enter their favourite hobby".center(70)
     hobby = gets.chomp
     
     students << {name: name.capitalize, cob: cob, age: age, hobby: hobby, cohort: :november}
-    puts "Now we have #{students.count} students"
+    puts "Now we have #{students.count} students".center(70)
     
-    puts "Enter next name"
+    puts "Enter next name".center(70)
     name = gets.chomp
 
   end
@@ -31,22 +31,22 @@ def input_students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "---------------"
+  puts "The students of Villains Academy".center(70)
+  puts "---------------".center(70)
 end
 
 def prints(students)
   index = 0
   while index < students.count do
     if students[index][:name][0] == "M" && students[index][:name].length < 12
-      puts "#{index + 1}. #{students[index][:name]} (#{students[index][:cohort]} cohort.)"
+      puts "#{index + 1}. #{students[index][:name]} (#{students[index][:cohort]} cohort.)".center(70)
     end
     index += 1
   end
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{students.count} great students".center(70)
 end
 
 students = input_students
