f810ca09830188dcbcd2d54f4dff5b01ca9f8102
Marina Papapetrou <contact@marinapapap.com>
2022-11-03 11:03:20 +0000

    Step 8 Ex 5

--- directory_copy.rb
+++ directory_copy.rb
@@ -1,6 +1,7 @@
 def input_students
-  puts "Please enter the names of the students"
+  puts "Please enter the names of the students, their country of birth, age and favourite hobby"
   puts "To finish, just hit return twice"
+  puts "First enter their name"
   
   students = []
   
@@ -8,10 +9,21 @@ def input_students
   
   while !name.empty? do
     
-    students << {name: name.capitalize, cohort: :november}
-    puts "now we have #{students.count} students"
+    puts "Enter their country of birth"
+    cob = gets.chomp
     
+    puts "Enter their age"
+    age = gets.chomp
+    
+    puts "Enter their favourite hobby"
+    hobby = gets.chomp
+    
+    students << {name: name.capitalize, cob: cob, age: age, hobby: hobby, cohort: :november}
+    puts "Now we have #{students.count} students"
+    
+    puts "Enter next name"
     name = gets.chomp
+
   end
   
   students
@@ -27,7 +39,7 @@ def prints(students)
   index = 0
   while index < students.count do
     if students[index][:name][0] == "M" && students[index][:name].length < 12
-      puts "#{index + 1}. #{students[index][:name]} (#{students[index][:cohort]} cohort.)".center(60)
+      puts "#{index + 1}. #{students[index][:name]} (#{students[index][:cohort]} cohort.)"
     end
     index += 1
   end
@@ -50,4 +62,9 @@ print_footer(students)
 #     end
 #   end
 
-#  || students[index][:name][0] == "m"
+
+    # students << {name: name.capitalize, cohort: :november}
+    # puts "now we have #{students.count} students"
+    
+    # name = gets.chomp
+    
