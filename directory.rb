845249aa0d681fea3cd7742c9ee23ff325579583
Marina Papapetrou <contact@marinapapap.com>
2022-11-04 13:23:33 +0000

    Taking arguments from the command line

--- directory.rb
+++ directory.rb
@@ -6,14 +6,14 @@ def input_students
   
   students = []
   
-  name = gets.chomp
+  name = STDIN.gets.chomp
   
   while !name.empty? do
     
     @students << {name: name, cohort: :november}
     puts "now we have #{@students.count} students"
     
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
   
   students
@@ -69,7 +69,7 @@ end
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -85,8 +85,8 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
   name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym}
@@ -94,4 +94,17 @@ def load_students
   file.close
 end
 
+def try_load_students
+  filename = ARGV.first # first argument from the command line
+  return if filename.nil? # get out of the method if it isn't given
+  if File.exist?(filename) # if it exists
+    load_students(filename)
+     puts "Loaded #{@students.count} from #{filename}"
+  else # if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist."
+    exit # quit the program
+  end
+end
+
+try_load_students
 interactive_menu
